fcc7f55f05f0525bd27d77c5a229122a7ea3deea
Haoming Wei <haoming.wei321@gmail.com>
2024-04-08 22:44:21 +0800

    fix(pro3-task1): fix bug in seq scan executor

--- src/execution/insert_executor.cpp
+++ src/execution/insert_executor.cpp
@@ -34,27 +34,33 @@ auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
   TableHeap *table = exec_ctx_->GetCatalog()->GetTable(plan_->TableOid())->table_.get();
   TupleMeta meta = {INVALID_TXN_ID, INVALID_TXN_ID, false};
   auto values_child_node = std::dynamic_pointer_cast<const ValuesPlanNode>(plan_->GetChildPlan());
-  if(values_child_node){
-    // 孩子节点属于values node类型
-    size = values_child_node->GetValues().size();
-    for(int i = 0; i < size; i++){
-      std::vector<Value> values;
-      int values_size = values_child_node->GetValues()[i].size();
-      for(int j = 0; j <  values_size; j++){
-        values.push_back(values_child_node->GetValues()[i][j]->Evaluate(tuple, exec_ctx_->GetCatalog()->GetTable(plan_->TableOid())->schema_));
-      }
-      const Schema *schema = &exec_ctx_->GetCatalog()->GetTable(plan_->TableOid())->schema_;
-      Tuple insert_tuple = Tuple(values, schema);
-      table->InsertTuple(meta, insert_tuple, nullptr, nullptr, plan_->TableOid());
-    }
-  } else {
-    // 孩子节点属于seq scan类型
-    RID child_rid;
-    Tuple child_tuple;
-    while (child_executor_->Next(&child_tuple, &child_rid)) {
-      size++;
-      table->InsertTuple(meta, child_tuple, nullptr, nullptr, plan_->TableOid());
-    }
+//  if(values_child_node){
+//    // 孩子节点属于values node类型
+//    size = values_child_node->GetValues().size();
+//    for(int i = 0; i < size; i++){
+//      std::vector<Value> values;
+//      int values_size = values_child_node->GetValues()[i].size();
+//      for(int j = 0; j <  values_size; j++){
+//        values.push_back(values_child_node->GetValues()[i][j]->Evaluate(tuple, exec_ctx_->GetCatalog()->GetTable(plan_->TableOid())->schema_));
+//      }
+//      const Schema *schema = &exec_ctx_->GetCatalog()->GetTable(plan_->TableOid())->schema_;
+//      Tuple insert_tuple = Tuple(values, schema);
+//      table->InsertTuple(meta, insert_tuple, nullptr, nullptr, plan_->TableOid());
+//    }
+//  } else {
+//    // 孩子节点属于seq scan类型
+//    RID child_rid;
+//    Tuple child_tuple;
+//    while (child_executor_->Next(&child_tuple, &child_rid)) {
+//      size++;
+//      table->InsertTuple(meta, child_tuple, nullptr, nullptr, plan_->TableOid());
+//    }
+//  }
+  RID child_rid;
+  Tuple child_tuple;
+  while (child_executor_->Next(&child_tuple, &child_rid)) {
+    size++;
+    table->InsertTuple(meta, child_tuple, nullptr, nullptr, plan_->TableOid());
   }
   std::vector<Value> res{};
   res.push_back(Value(TypeId::INTEGER, size));
--- src/execution/update_executor.cpp
+++ src/execution/update_executor.cpp
@@ -24,7 +24,28 @@ void UpdateExecutor::Init() {
 }
 
 auto UpdateExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
-  return false;
+  if(is_updated){
+    return false;
+  }
+  TableHeap *table = table_info_->table_.get();
+  int size = 0;
+  RID child_rid;
+  Tuple child_tuple;
+  TupleMeta meta = {INVALID_TXN_ID, INVALID_TXN_ID, false};
+  while (child_executor_->Next(&child_tuple, &child_rid)) {
+    size++;
+    // 删除旧数据
+    TupleMeta old_meta = table->GetTupleMeta(child_rid);
+    old_meta.is_deleted_ = true;
+    table->UpdateTupleMeta(old_meta, child_rid);
+    // 插入新数据
+    table->InsertTuple(meta, child_tuple, nullptr, nullptr, plan_->TableOid());
+  }
+  std::vector<Value> res{};
+  res.push_back(Value(TypeId::INTEGER, size));
+  *tuple = Tuple{res, &GetOutputSchema()};
+  is_updated = true;
+  return true;
 }
 
 }  // namespace bustub
