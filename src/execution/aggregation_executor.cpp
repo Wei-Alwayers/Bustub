716ad51761cbbaf9a96e473e8656b8f45c8f69f4
Haoming Wei <haoming.wei321@gmail.com>
2024-04-09 17:54:29 +0800

    feat(pro3-task2): implement aggregation executor

--- src/execution/aggregation_executor.cpp
+++ src/execution/aggregation_executor.cpp
@@ -18,11 +18,34 @@ namespace bustub {
 
 AggregationExecutor::AggregationExecutor(ExecutorContext *exec_ctx, const AggregationPlanNode *plan,
                                          std::unique_ptr<AbstractExecutor> &&child)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), child_(std::move(child)), aht_(plan->GetAggregates(), plan->GetAggregateTypes()), aht_iterator_(aht_.Begin()){}
 
-void AggregationExecutor::Init() {}
+void AggregationExecutor::Init() {
+  RID child_rid;
+  Tuple child_tuple;
+  child_->Init();
+  while (child_->Next(&child_tuple, &child_rid)) {
+    aht_.InsertCombine(MakeAggregateKey(&child_tuple), MakeAggregateValue(&child_tuple));
+  }
+  if(aht_.Size() == 0 && plan_->group_bys_.empty()){
+    aht_.Init();
+  }
+  aht_iterator_ = aht_.Begin();
+}
 
-auto AggregationExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto AggregationExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  if(aht_iterator_ == aht_.End()){
+    return false;
+  }
+  std::vector<Value> values = aht_iterator_.Val().aggregates_;
+  std::vector<Value> key_values = aht_iterator_.Key().group_bys_;
+  for(auto value :  values){
+    key_values.push_back(value);
+  }
+  *tuple = Tuple{key_values, &GetOutputSchema()};
+  aht_iterator_.operator++();
+  return true;
+}
 
 auto AggregationExecutor::GetChildExecutor() const -> const AbstractExecutor * { return child_.get(); }
 
